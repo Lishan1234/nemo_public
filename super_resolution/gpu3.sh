8bb0442f1859c6de1a0cb27b3217501d36a1ec80
hyunho <chaos5958@gmail.com>
2019-01-28 11:21:03 +0900

    Minor

--- super_resolution/gpu3.sh
+++ super_resolution/gpu3.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-#CUDA_VISIBLE_DEVICES=3 python train.py --load_on_memory --num_blocks 2 --num_filters 64 --upsample_type transpose --conv_type depthwise_v3
-CUDA_VISIBLE_DEVICES=3 python train.py --load_on_memory --num_blocks 5 --num_filters 64 --upsample_type transpose --conv_type depthwise_v3
+#CUDA_VISIBLE_DEVICES=3 python train.py --scale 4 --num_patch 50000 --train_data soccer --valid_data soccer --data_type keyframe --data_dir /ssd1 
+CUDA_VISIBLE_DEVICES=3 python test.py --scale 4 --train_data soccer --valid_data soccer --data_type 4sec --data_dir data --lr 270
