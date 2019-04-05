6186389b28ae3fa7e5792ddb52450c0a254cf0ee
Hyunho Yeo <chaos5958@gmail.com>
2019-04-05 18:22:34 +0900

    Minor

--- TestLibvpx/app/src/main/jni/setup_libvpx.py
+++ TestLibvpx/app/src/main/jni/setup_libvpx.py
@@ -3,18 +3,33 @@ import sys
 import argparse
 from shutil import copyfile
 
-SOURCE = os.path.abspath('../../../../../libvpx')
-TARGET = os.path.abspath('libvpx')
-
 parser = argparse.ArgumentParser(description='setup_libvpx')
 parser.add_argument('--abi', type=str, default='arm64-v8a')
 args = parser.parse_args()
 
+if os.path.isfile('libvpx'):
+    os.remove('libvpx')
+    os.symlink('../../../../../libvpx', 'libvpx')
+else:
+    os.symlink('../../../../../libvpx', 'libvpx')
+
+config_file = 'libvpx_android_configs/{}/vpx_config.h'.format(args.abi)
+assert os.path.isfile(config_file)
+if os.path.isfile('vpx_config.h'):
+    os.remove('vpx_config.h')
+    os.symlink(config_file, 'vpx_config.h')
+else:
+    os.symlink(config_file, 'vpx_config.h')
+
+
+"""
+SOURCE = os.path.abspath('../../../../../libvpx')
+TARGET = os.path.abspath('libvpx')
+
+
 srcs_file = 'libvpx_android_configs/{}/libvpx_srcs.txt'.format(args.abi)
 srcs_file_ = 'libvpx_android_configs/libvpx_srcs.txt'.format(args.abi)
-config_file = 'libvpx_android_configs/{}/vpx_config.h'.format(args.abi)
 assert os.path.isfile(srcs_file)
-assert os.path.isfile(config_file)
 
 with open(srcs_file) as config:
     for line in config:
@@ -47,7 +62,5 @@ with open(srcs_file_) as config:
         os.makedirs(os.path.dirname(dest), exist_ok=True)
         os.symlink(src,dest)
         #copyfile(src, dest) - replace by symbolic links
+"""
 
-if os.path.isfile('vpx_config.h'):
-    os.remove('vpx_config.h')
-    os.symlink(config_file, 'vpx_config.h')
