b35653044305cc8b3a895407d7f76dd17fe54c2b
Martin <martinc2907@gmail.com>
2019-06-09 22:59:54 +0900

    per-minute logging & video repeat done

--- BatteryProfiler/app/src/main/java/com/example/batteryprofiler/ExoPlayer.java
+++ BatteryProfiler/app/src/main/java/com/example/batteryprofiler/ExoPlayer.java
@@ -6,6 +6,8 @@ import android.net.Uri;
 import android.os.BatteryManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -19,8 +21,10 @@ import android.widget.Toast;
 
 import com.google.android.exoplayer2.DefaultLoadControl;
 import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.PlayerMessage;
 import com.google.android.exoplayer2.SimpleExoPlayer;
 import com.google.android.exoplayer2.analytics.AnalyticsListener;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
@@ -40,7 +44,6 @@ public class ExoPlayer extends AppCompatActivity {
 
     PlayerView mPlayerView;
     SimpleExoPlayer mSimpleExoPlayer;
-    BatteryManager mBatteryManager;
     Long start_battery;
     Long end_battery;
     int start_percentage;
@@ -61,31 +64,16 @@ public class ExoPlayer extends AppCompatActivity {
 
         prep_exo();
 
-        mBatteryManager = (BatteryManager) getApplicationContext().getSystemService(getApplicationContext().BATTERY_SERVICE);
-
-        //get current battery
-        start_battery = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER)/1000;
-        start_percentage = mBatteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
-        Log.i("TAGG",start_battery + "mAh");
-        Log.i("TAGG",start_percentage + "%");
-
         //listener for end battery
         mSimpleExoPlayer.addListener(new Player.EventListener(){
             @Override
             public void onPlayerStateChanged(boolean playWhenReady, int playbackState){
+
                 switch(playbackState){
                     case Player.STATE_ENDED:
-                        end_battery = mBatteryManager.getLongProperty(BatteryManager.BATTERY_PROPERTY_CHARGE_COUNTER);
-                        end_percentage = mBatteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
-                        Log.i("TAGG",end_battery+"mAh");
-                        Log.i("TAGG",end_percentage + "%");
 
                         //finish and go back to previous activity
                         Intent data = new Intent();
-                        data.putExtra("start bat",start_battery);
-                        data.putExtra("end bat", end_battery);
-                        data.putExtra("start percentage",start_percentage);
-                        data.putExtra("end percentage", end_percentage);
                         setResult(RESULT_OK,data);
                         finish();
                         break;
@@ -104,7 +92,6 @@ public class ExoPlayer extends AppCompatActivity {
             }
         });
 
-
         mSimpleExoPlayer.setPlayWhenReady(true);
     }
 
@@ -120,6 +107,10 @@ public class ExoPlayer extends AppCompatActivity {
         else if(intent.getIntExtra("requestcode",0)==3){
             video = "https://storage.googleapis.com/exoplayer-test-media-1/gen-3/screens/dash-vod-single-segment/video-vp9-1080.webm";
         }
+        else if(intent.getIntExtra("requestcode",0)==4){
+            video = "https://storage.googleapis.com/exoplayer-test-media-1/gen-3/screens/dash-vod-single-segment/video-vp9-1080.webm";
+        }
+
 
         //create player and set it to view
         mPlayerView = findViewById(R.id.video_view);
@@ -146,6 +137,7 @@ public class ExoPlayer extends AppCompatActivity {
 
         //use media source
         mSimpleExoPlayer.prepare(mediaSource);
+
     }
 
     private MediaSource createMediaSource(Uri uri){
