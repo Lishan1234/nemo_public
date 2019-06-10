a9547b92c8aacb49e9e0d0eb1110669fb427f0d8
Martin <martinc2907@gmail.com>
2019-06-10 20:48:10 +0900

    Finish SNPE battery app

---
+++ android_app/snpe_battery/app/src/main/java/com/example/snpesample/OnTaskCompleted.java
@@ -0,0 +1,5 @@
+package com.example.snpesample;
+
+public interface OnTaskCompleted {
+    void onTaskCompleted();
+}
---
+++ android_app/snpe_battery/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/obj/local/armeabi-v7a/objs/snpe-sample/CreateGLBuffer.o.d
@@ -0,0 +1,62 @@
+/home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/obj/local/armeabi-v7a/objs/snpe-sample/CreateGLBuffer.o: \
+ /home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/CreateGLBuffer.cpp \
+ /home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/string \
+ /home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream \
+ /home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+ /home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/CreateGLBuffer.hpp \
+ /home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/sstream \
+ /home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/fstream \
+ /home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdlib \
+ /home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/Util.hpp \
+ /home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/ITensorFactory.hpp \
+ /home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/ITensor.hpp \
+ /home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/ITensorItr.hpp \
+ /home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/ZdlExportDefine.hpp \
+ /home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/ITensorItrImpl.hpp \
+ /home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/memory \
+ /home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/iterator \
+ /home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/TensorShape.hpp \
+ /home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath \
+ /home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/TensorShape.hpp \
+ /home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/PlatformConfig.hpp \
+ /home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/ZdlExportDefine.hpp
+
+/home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/string:
+
+/home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream:
+
+/home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
+
+/home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/CreateGLBuffer.hpp:
+
+/home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/sstream:
+
+/home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/fstream:
+
+/home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdlib:
+
+/home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/Util.hpp:
+
+/home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/ITensorFactory.hpp:
+
+/home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/ITensor.hpp:
+
+/home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/ITensorItr.hpp:
+
+/home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/ZdlExportDefine.hpp:
+
+/home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/ITensorItrImpl.hpp:
+
+/home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/memory:
+
+/home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/iterator:
+
+/home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/TensorShape.hpp:
+
+/home/intern/AndroidStudioProjects/android-ndk-r15c/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath:
+
+/home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/TensorShape.hpp:
+
+/home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/PlatformConfig.hpp:
+
+/home/intern/AndroidStudioProjects/SNPEsample/app/src/main/jni/snpe/examples/NativeCpp/SampleCode/jni/../../../../include/zdl/DlSystem/ZdlExportDefine.hpp:
