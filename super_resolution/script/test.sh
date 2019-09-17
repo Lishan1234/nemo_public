f2ae0dc0fa65e357051c7f3af38d88a212647a41
Hyunho Yeo <chaos5958@gmail.com>
2019-09-17 09:25:04 +0900

    Minor

--- super_resolution/script/test.sh
+++ super_resolution/script/test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-#python setup_videos.py --dataset starcraft --scale 4 --video_start 120 --lq_dnn EDSR_transpose_B8_F8 --hq_dnn EDSR_transpose_B8_F32
-python setup_videos.py --dataset movie --scale 4 --video_start 125 --lq_dnn EDSR_transpose_B8_F8 --hq_dnn EDSR_transpose_B8_F32
+python setup_videos.py --dataset starcraft --scale 4 --video_start 120 --lq_dnn EDSR_transpose_B8_F8 --hq_dnn EDSR_transpose_B8_F32
+#python setup_videos.py --dataset movie --scale 4 --video_start 125 --lq_dnn EDSR_transpose_B8_F8 --hq_dnn EDSR_transpose_B8_F32
 #python setup_videos.py --dataset basketball --scale 4 --video_start 1090 --lq_dnn EDSR_transpose_B8_F8 --hq_dnn EDSR_transpose_B8_F32
 
 #python setup_graphs.py --dataset starcraft --scale 4 --video_start 120 --lq_dnn EDSR_transpose_B8_F8 --hq_dnn EDSR_transpose_B8_F32 --start_idx 0 --end_idx 500
