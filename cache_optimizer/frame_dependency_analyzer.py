ff175ac92720c4fb7f082cbaef6b0e86f0813918
Hyunho Yeo <chaos5958@gmail.com>
2019-12-26 13:22:30 +0900

    FDA: construct DAG graph

--- cache_optimizer/frame_dependency_analyzer.py
+++ cache_optimizer/frame_dependency_analyzer.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import argparse
+import networkx as nx
 
 #Assumption: Fixed GOP interval
 class FDA():
@@ -23,8 +24,42 @@ class FDA():
         print('FDA executes {}'.format(cmd))
         os.system(cmd)
 
+    def analyze(self, chunk_idx):
+        postfix = 'chunk{:04d}'.format(chunk_idx)
+        log_path = os.path.join(self.content_dir, 'log', self.input_video, postfix, 'metadata.txt')
+
+        G = nx.DiGraph()
+        with open(log_path, 'r') as f:
+            lines = f.readlines()
+            for idx, line in enumerate(lines):
+                result = line.strip().split('\t')
+                video_frame = int(result[0])
+                super_frame = int(result[1])
+                node_name = '{}.{}'.format(video_frame, super_frame)
+
+                if len(result) == 11:
+                    #add node
+                    G.add_node(node_name, video_frame=video_frame, super_frame=super_frame)
+
+                    #add edge
+                    for i in range(3):
+                        ref_video_frame = int(result[2*i+5])
+                        ref_super_frame = int(result[2*i+6])
+                        ref_node_name = '{}.{}'.format(ref_video_frame, ref_super_frame)
+                        G.add_edge(node_name, ref_node_name)
+                else:
+                    #add node
+                    G.add_node(node_name, video_frame=video_frame, super_frame=super_frame)
+
+        #2. log a DAG data
+        #iterate over node
+        #if node has out_degree >= 2, log into queue1
+        #if node has out_degree == 1, log into queue2
+
+        #3. visualize a DAG
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Cache optimizer')
+    parser = argparse.ArgumentParser(description='Frame Dependency Analyzer')
 
     #options for libvpx
     parser.add_argument('--vpxdec_path', type=str, required=True)
@@ -37,4 +72,5 @@ if __name__ == '__main__':
 
     fda = FDA(os.path.abspath(args.vpxdec_path), os.path.abspath(args.content_dir),
                 args.input_video_name, args.num_threads, args.gop)
-    fda.run(0)
+    #fda.run(0)
+    fda.analyze(0)
