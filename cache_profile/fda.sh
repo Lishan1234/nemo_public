f0f95e7ee1933df10a952a5b408600510724860f
root <chaos5958@gmail.com>
2020-01-29 13:52:35 +0900

    FDA: Finish

--- cache_profile/fda.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-python frame_dependency_analyzer.py --vpxdec_path /home/hyunho/MobiNAS/third_party/libvpx/vpxdec \
-                                --content_dir /ssd1/data-sigcomm2020/game-lol \
-                                --input_video_name 240p_s0_d60_encoded.webm \
-                                --num_threads 1 \
-                                --gop 120 \
-                                --chunk_idx 5 \
-                                --num_visualized_frames 30 
--- cache_profile/frame_dependency_analyzer.py
+++ cache_profile/frame_dependency_analyzer.py
@@ -1,44 +1,64 @@
 import os
 import sys
 import argparse
+import collections
+
 import networkx as nx
 import matplotlib.pyplot as plt
 
+from tool.video import profile_video
+from tool.libvpx import libvpx_save_metadata
+
 #Assumption: Fixed GOP interval
 #TODO: set proper number of threads as CRA
+#TODO: run()
+#TODO: script/{}
+#TODO: show_super_frame
 class FDA():
-    def __init__(self, vpxdec_path, content_dir, input_video, num_threads, gop, num_visualized_frames):
-        self.vpxdec_path = vpxdec_path
-        self.content_dir = content_dir
-        self.input_video = input_video
-        self.num_threads = num_threads
+    def __init__(self, vpxdec_file, dataset_dir, video_name, gop, num_visualized_frames, show_super_frame):
+        self.vpxdec_file = vpxdec_file
+        self.dataset_dir = dataset_dir
+        self.video_name = video_name
         self.gop = gop
         self.num_visualized_frames = num_visualized_frames
+        self.show_super_frame = show_super_frame
 
-    def decode(self, chunk_idx):
-        start_idx = chunk_idx * self.gop
-        end_idx = (chunk_idx + 1) * self.gop
+    def node_name(self, frame_index, node_index_dict):
+        if not self.show_super_frame:
+            node_name = '{}'.format(node_index_dict[frame_index])
+        else:
+            node_name = '{}.{}'.format(frame_index[0], frame_index[1])
+        return  node_name
+
+    def run(self, chunk_idx):
+        #decode
+        libvpx_save_metadata(self.vpxdec_file, self.dataset_dir, self.video_name, self.gop, chunk_idx)
+
+        #load metadata
         postfix = 'chunk{:04d}'.format(chunk_idx)
+        metadata_log_file = os.path.join(self.dataset_dir, 'log', self.video_name, postfix, 'metadata.txt')
 
-        #execute
-        cmd = '{} --codec=vp9 --noblit --threads={} --frame-buffers=50 --skip={} --limit={} \
-                --content-dir={} --input-video={} --postfix={} --save-metadata'.format(self.vpxdec_path, self.num_threads, \
-                start_idx, end_idx - start_idx, self.content_dir, self.input_video, postfix)
-        print('FDA executes {}'.format(cmd))
-        os.system(cmd)
+        #node index
+        node_index_dict = {}
+        count = 0
+        with open(metadata_log_file, 'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                line = line.strip().split('\t')
+                video_frame = int(line[0])
+                super_frame = int(line[1])
+                node_index_dict[(video_frame, super_frame)] = count
+                count += 1
 
-    def log_graph(self, chunk_idx):
-        postfix = 'chunk{:04d}'.format(chunk_idx)
-        metadata_log_path = os.path.join(self.content_dir, 'log', self.input_video, postfix, 'metadata.txt')
-
+        #build a DAG
         G = nx.DiGraph()
-        with open(metadata_log_path, 'r') as f:
+        with open(metadata_log_file, 'r') as f:
             lines = f.readlines()
             for idx, line in enumerate(lines):
                 result = line.strip().split('\t')
                 video_frame = int(result[0])
                 super_frame = int(result[1])
-                node_name = '{}.{}'.format(video_frame, super_frame)
+                node_name = self.node_name((video_frame, super_frame), node_index_dict)
 
                 if len(result) == 11:
                     #add node
@@ -48,40 +68,45 @@ class FDA():
                     for i in range(3):
                         ref_video_frame = int(result[2*i+5])
                         ref_super_frame = int(result[2*i+6])
-                        ref_node_name = '{}.{}'.format(ref_video_frame, ref_super_frame)
+                        ref_node_name = self.node_name((ref_video_frame, ref_super_frame), node_index_dict)
                         G.add_edge(ref_node_name, node_name)
                 else:
                     #add node
                     G.add_node(node_name, video_frame=video_frame, super_frame=super_frame)
 
-        postfix = 'chunk{:04d}'.format(chunk_idx)
-        nodes = sorted(G.nodes, key=lambda x: float(x))
-        queue1_log_path = os.path.join(self.content_dir, 'log', self.input_video, postfix, 'queue1.txt')
-        queue2_log_path = os.path.join(self.content_dir, 'log', self.input_video, postfix, 'queue2.txt')
-
-        with open(queue1_log_path, 'w') as f1, open(queue2_log_path, 'w') as f2:
+        #log0: out-degree per frame index
+        if self.show_super_frame:
+            nodes = sorted(G.nodes, key=lambda x: float(x))
+        else:
+            nodes = sorted(G.nodes, key=lambda x: int(x))
+        nodes = G.nodes
+        log_file = os.path.join(self.dataset_dir, 'log', self.video_name, postfix, 'out_degree_per_frame.txt')
+        with open(log_file, 'w') as f:
             for node in nodes:
-                print(node, G.out_degree(node), list(G.successors(node)))
+                #print(node, G.out_degree(node), list(G.successors(node)))
                 log = '{}\t{}\t{}\n'.format(G.nodes[node]['video_frame'], G.nodes[node]['super_frame'], G.out_degree(node))
-                if G.out_degree(node) >= 2:
-                    f1.write(log)
-                else:
-                    f2.write(log)
+                f.write(log)
 
-    #TODO: add options for draw_spectral() (more clear nodes, edges)
-    #Reference: https://networkx.github.io/documentation/stable/tutorial.html#drawing-graphs
-    def visualize_graph(self, chunk_idx):
-        postfix = 'chunk{:04d}'.format(chunk_idx)
-        metadata_log_path = os.path.join(self.content_dir, 'log', self.input_video, postfix, 'metadata.txt')
+        #log1: out-degree histogram
+        degree_sequence = sorted([d for n, d in G.out_degree()], reverse=True)
+        degreeCount = collections.Counter(degree_sequence)
+        deg, cnt = zip(*degreeCount.items())
+        log_file = os.path.join(self.dataset_dir, 'log', self.video_name, postfix, 'out_degree_histogram.txt')
+        with open(log_file, 'w') as f:
+            for deg, cnt in zip(deg, cnt):
+                log = '{}\t{}\n'.format(deg, cnt)
+                f.write(log)
 
+        #build a subgraph
+        del(G)
         G = nx.DiGraph()
-        with open(metadata_log_path, 'r') as f:
+        with open(metadata_log_file, 'r') as f:
             lines = f.readlines()
             for idx, line in enumerate(lines):
                 result = line.strip().split('\t')
                 video_frame = int(result[0])
                 super_frame = int(result[1])
-                node_name = '{}.{}'.format(video_frame, super_frame)
+                node_name = self.node_name((video_frame, super_frame), node_index_dict)
 
                 if len(result) == 11:
                     #add node
@@ -91,7 +116,7 @@ class FDA():
                     for i in range(3):
                         ref_video_frame = int(result[2*i+5])
                         ref_super_frame = int(result[2*i+6])
-                        ref_node_name = '{}.{}'.format(ref_video_frame, ref_super_frame)
+                        ref_node_name = self.node_name((ref_video_frame, ref_super_frame), node_index_dict)
                         G.add_edge(ref_node_name, node_name)
                 else:
                     #add node
@@ -100,49 +125,58 @@ class FDA():
                 if self.num_visualized_frames is not None and idx + 1 >= self.num_visualized_frames:
                     break
 
-        options = {
-                'node_color': 'black',
-                'node_size': 10,
-                'width': 1,
-                }
-        graph_path = os.path.join(self.content_dir, 'log', self.input_video, postfix, 'graph.png')
-        nx.draw(G, **options)
-        plt.savefig(graph_path)
+        #visualize a DAG
+        pos = nx.spring_layout(G)
+        nx.draw_networkx_nodes(G, pos, node_size=700)
+        nx.draw_networkx_edges(G, pos, width=4, arrowsize=15)
+        nx.draw_networkx_labels(G, pos, font_size=17, font_family='sans-serif')
+        graph_file = os.path.join(self.dataset_dir, 'log', self.video_name, postfix, 'graph_spring.png')
+        plt.savefig(graph_file)
         plt.clf()
 
-        s_graph_path = os.path.join(self.content_dir, 'log', self.input_video, postfix, 'graph_spectral.png')
-        nx.draw_spectral(G, **options)
-        plt.savefig(s_graph_path)
+        pos = nx.spectral_layout(G)
+        """
+        #hardcode for paper figure
+        pos['0'] = [-0.3, 0.6]
+        pos['1'] = [0.7,  0.6]
+        pos['2'] = [-1.2, 0.0]
+        pos['3'] = [-0.8, 0.0]
+        pos['4'] = [-0.4, 0.0]
+        pos['5'] = [0.0, 0.0]
+        pos['6'] = [0.4, 0.0]
+        pos['7'] = [0.8, 0.0]
+        pos['8'] = [1.2, 0.0]
+        pos['9'] = [1.6, 0.0]
+        """
+        nx.draw_networkx_nodes(G, pos, node_size=700)
+        nx.draw_networkx_edges(G, pos, width=4, arrowsize=15)
+        nx.draw_networkx_labels(G, pos, font_size=17, font_family='sans-serif')
+        graph_file = os.path.join(self.dataset_dir, 'log', self.video_name, postfix, 'graph_spectral.png')
+        plt.savefig(graph_file)
         plt.clf()
-
-    def run(self, chunk_idx=None):
-        if chunk_idx is None:
-            input_video_path = os.path.join(self.content_dir, 'video', self.input_video)
-            input_video_info = profile_video(input_video_path)
-            num_chunks = int(input_video_info['duration'] // (self.gop / input_video_info['frame_rate']))
-            for i in range(num_chunks):
-                self.decode(i)
-                self.log_graph(i)
-                self.visualize_graph(i)
-        else:
-            self.decode(chunk_idx)
-            self.log_graph(chunk_idx)
-            self.visualize_graph(chunk_idx)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Frame Dependency Analyzer')
 
     #options for libvpx
-    parser.add_argument('--vpxdec_path', type=str, required=True)
-    parser.add_argument('--content_dir', type=str, required=True)
-    parser.add_argument('--input_video_name', type=str, required=True)
-    parser.add_argument('--num_threads', type=int, default=1)
+    parser.add_argument('--vpxdec_file', type=str, required=True)
+    parser.add_argument('--dataset_dir', type=str, required=True)
+    parser.add_argument('--video_name', type=str, required=True)
     parser.add_argument('--gop', type=int, required=True)
     parser.add_argument('--chunk_idx', type=int, default=None)
     parser.add_argument('--num_visualized_frames', type=int, default=None)
+    parser.add_argument('--show_super_frame', type=bool, default=False)
 
     args = parser.parse_args()
 
-    fda = FDA(os.path.abspath(args.vpxdec_path), os.path.abspath(args.content_dir),
-                args.input_video_name, args.num_threads, args.gop, args.num_visualized_frames)
-    fda.run(args.chunk_idx)
+    video_file = os.path.join(args.dataset_dir, 'video', args.video_name)
+    video_info = profile_video(video_file)
+
+    fda = FDA(os.path.abspath(args.vpxdec_file), os.path.abspath(args.dataset_dir), args.video_name, args.gop, \
+                args.num_visualized_frames, args.show_super_frame)
+    if args.chunk_idx is None:
+        num_chunks = int(video_info['duration'] // (args.gop / video_info['frame_rate']))
+        for i in range(num_chunks):
+            fda.run(args.chunk_idx)
+    else:
+        fda.run(args.chunk_idx)
