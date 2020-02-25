f422d2ec449971e029c0f1c5429b6d4487f098ac
root <chaos5958@gmail.com>
2020-02-25 18:38:06 +0900

    Minor

--- evaluation/libvpx_results.py
+++ evaluation/libvpx_results.py
@@ -12,3 +12,16 @@ def libvpx_quality(log_dir):
                 quality.append(float(quality_line[1]))
 
         return quality
+
+def libvpx_latency(log_dir):
+        latency_log_file = os.path.join(log_dir, 'latency.txt')
+        latency = []
+
+        with open(latency_log_file, 'r') as f:
+            latency_lines = f.readlines()
+
+            for latency_line in latency_lines:
+                latency_line = latency_line.strip().split('\t')
+                latency.append(float(latency_line[2]))
+
+        return latency
---
+++ evaluation/nemo_s/script/eval01_02_a.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+python $MOBINAS_CODE_ROOT/evaluation/nemo_s/eval01_02_a.py \
+                --dataset_rootdir $MOBINAS_DATA_ROOT \
+                --content how_to \
+                --lr_resolution 240 \
+                --hr_resolution 960 \
+                --num_blocks 8 \
+                --num_filters 32 \
+                --baseline_num_blocks 8 \
+                --baseline_num_filters 32 \
+                --upsample_type deconv \
+                --threshold 0.5 \
+                --aps_class nemo \
+                --device_id a152b92a 
