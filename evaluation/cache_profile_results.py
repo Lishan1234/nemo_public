97632f7364d4922f2c9cdaca6ab39fffd21539e7
Hyunho Yeo <chaos5958@gmail.com>
2020-02-29 00:29:20 -0500

    Updated evaluation code

--- evaluation/cache_profile_results.py
+++ evaluation/cache_profile_results.py
@@ -1,57 +1,72 @@
 import os
 import numpy as np
 
+def chunk_quality(log_dir):
+    quality_log_file = os.path.join(log_dir, 'quality.txt')
+    quality_cache = []
+    quality_dnn = None
+
+    with open(quality_log_file, 'r') as f:
+        quality_lines = f.readlines()
+
+        for quality_line in quality_lines:
+            quality_line = quality_line.strip().split('\t')
+            quality_cache.append(float(quality_line[1]))
+            quality_dnn = float(quality_line[2])
+
+    return quality_cache, quality_dnn
+
 def quality(log_dir):
-        quality_log_file = os.path.join(log_dir, 'quality.txt')
-        quality_cache = []
-        quality_dnn = []
-        quality_bilinear = []
+    quality_log_file = os.path.join(log_dir, 'quality.txt')
+    quality_cache = []
+    quality_dnn = []
+    quality_bilinear = []
 
-        with open(quality_log_file, 'r') as f:
-            quality_lines = f.readlines()
+    with open(quality_log_file, 'r') as f:
+        quality_lines = f.readlines()
 
-            for quality_line in quality_lines:
-                quality_line = quality_line.strip().split('\t')
-                quality_cache.append(float(quality_line[2]))
-                quality_dnn.append(float(quality_line[3]))
-                quality_bilinear.append(float(quality_line[4]))
+        for quality_line in quality_lines:
+            quality_line = quality_line.strip().split('\t')
+            quality_cache.append(float(quality_line[2]))
+            quality_dnn.append(float(quality_line[3]))
+            quality_bilinear.append(float(quality_line[4]))
 
-        result_cache = {}
-        result['avg_quality'] = np.round(np.average(quality_cache), 4)
-        result['std_quality'] = np.round(np.std(quality_cache), 4)
-        result_dnn = {}
-        result['avg_quality'] = np.round(np.average(quality_dnn), 4)
-        result['std_quality'] = np.round(np.std(quality_dnn), 4)
-        result_bilinear = {}
-        result['avg_quality'] = np.round(np.average(quality_bilinear), 4)
-        result['std_quality'] = np.round(np.std(quality_bilinear), 4)
+    result_cache = {}
+    result['avg_quality'] = np.round(np.average(quality_cache), 4)
+    result['std_quality'] = np.round(np.std(quality_cache), 4)
+    result_dnn = {}
+    result['avg_quality'] = np.round(np.average(quality_dnn), 4)
+    result['std_quality'] = np.round(np.std(quality_dnn), 4)
+    result_bilinear = {}
+    result['avg_quality'] = np.round(np.average(quality_bilinear), 4)
+    result['std_quality'] = np.round(np.std(quality_bilinear), 4)
 
-        return result_cache, result_dnn, result_bilinear
+    return result_cache, result_dnn, result_bilinear
 
 def quality_gain(log_dir):
-        quality_log_file = os.path.join(log_dir, 'quality.txt')
-        quality_gain_cache = []
-        quality_gain_dnn = []
+    quality_log_file = os.path.join(log_dir, 'quality.txt')
+    quality_gain_cache = []
+    quality_gain_dnn = []
 
-        with open(quality_log_file, 'r') as f:
-            quality_lines = f.readlines()
+    with open(quality_log_file, 'r') as f:
+        quality_lines = f.readlines()
 
-            for quality_line in quality_lines:
-                quality_line = quality_line.strip().split('\t')
-                quality_cache = float(quality_line[2])
-                quality_dnn = float(quality_line[3])
-                quality_bilinear = float(quality_line[4])
-                quality_gain_cache.append(quality_cache - quality_bilinear)
-                quality_gain_dnn.append(quality_dnn - quality_bilinear)
+        for quality_line in quality_lines:
+            quality_line = quality_line.strip().split('\t')
+            quality_cache = float(quality_line[2])
+            quality_dnn = float(quality_line[3])
+            quality_bilinear = float(quality_line[4])
+            quality_gain_cache.append(quality_cache - quality_bilinear)
+            quality_gain_dnn.append(quality_dnn - quality_bilinear)
 
-        result_cache = {}
-        result['avg_quality'] = np.round(np.average(quality_cache_gain), 4)
-        result['std_quality'] = np.round(np.std(quality_cache_gain), 4)
-        result_dnn = {}
-        result['avg_quality'] = np.round(np.average(quality_dnn_gain), 4)
-        result['std_quality'] = np.round(np.std(quality_dnn_gain), 4)
+    result_cache = {}
+    result['avg_quality'] = np.round(np.average(quality_cache_gain), 4)
+    result['std_quality'] = np.round(np.std(quality_cache_gain), 4)
+    result_dnn = {}
+    result['avg_quality'] = np.round(np.average(quality_dnn_gain), 4)
+    result['std_quality'] = np.round(np.std(quality_dnn_gain), 4)
 
-        return result_cache, result_dnn, result_bilinear
+    return result_cache, result_dnn, result_bilinear
 
 def norm_mac(log_dir):
     mac_log_file = os.path.join(log_dir, 'mac.txt')
