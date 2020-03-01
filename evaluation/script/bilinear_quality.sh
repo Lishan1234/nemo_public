6d74a41f78e9f01de7e9d515c6ced1e1aa571283
Hyunho Yeo <chaos5958@gmail.com>
2020-03-01 09:05:51 -0500

    Fix a bug in selecting a hr video

--- evaluation/script/bilinear_quality.sh
+++ evaluation/script/bilinear_quality.sh
@@ -2,8 +2,8 @@
 
 python $MOBINAS_CODE_ROOT/evaluation/bilinear_quality.py \
     --dataset_rootdir $MOBINAS_DATA_ROOT \
-    --content education \
-    --lr_resolution 240 \
+    --content unboxing \
+    --lr_resolution 1080 \
     --hr_resolution 1080 \
     --ffmpeg_file /usr/bin/ffmpeg \
     --vpxdec_file $MOBINAS_CODE_ROOT/third_party/libvpx/vpxdec
