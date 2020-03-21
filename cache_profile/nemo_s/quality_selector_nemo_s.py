8f509925497cd34550c260606c9345e980c767e0
root <chaos5958@gmail.com>
2020-03-21 21:09:06 +0900

    Minor

--- cache_profile/nemo_s/quality_selector_nemo_s.py
+++ cache_profile/nemo_s/quality_selector_nemo_s.py
@@ -3,6 +3,7 @@ import os
 import math
 import glob
 import numpy as np
+import json
 
 import tensorflow as tf
 
@@ -40,7 +41,7 @@ if __name__ == '__main__':
     parser.add_argument('--gop', type=int, required=True)
 
     #device
-    parser.add_argument('--device_id', type=str, required=True)
+    parser.add_argument('--device_id', type=str, nargs='+', required=True)
 
     args = parser.parse_args()
 
@@ -56,56 +57,73 @@ if __name__ == '__main__':
     #reference video
     cache_profile_name = '{}_{}.profile'.format(APS_NEMO.NAME1, args.threshold)
     latency_dict = {}
-    for model in models:
-        anchor_point = []
-        non_anchor_frame = []
 
-        video_file = os.path.abspath(glob.glob(os.path.join(args.dataset_rootdir, args.reference_content,  'video', '{}p*'.format(args.lr_resolution)))[0])
-        video_name = os.path.basename(video_file)
-        latency_log = os.path.join(args.dataset_rootdir, args.reference_content, 'log', video_name, model.name, cache_profile_name, args.device_id, 'latency.txt')
-        metadata_log = os.path.join(args.dataset_rootdir, args.reference_content, 'log', video_name, model.name, cache_profile_name, args.device_id, 'metadata.txt')
+    for device_id in args.device_id:
+        latency_dict[device_id] = {}
 
-        with open(latency_log, 'r') as f0, open(metadata_log, 'r') as f1:
-            latency_lines = f0.readlines()
-            metadata_lines = f1.readlines()
+        for model in models:
+            anchor_point = []
+            non_anchor_frame = []
 
-            for latency_line, metadata_line in zip(latency_lines, metadata_lines):
-                latency_result = latency_line.strip().split('\t')
-                metadata_result = metadata_line.strip().split('\t')
+            video_file = os.path.abspath(glob.glob(os.path.join(args.dataset_rootdir, args.reference_content,  'video', '{}p*'.format(args.lr_resolution)))[0])
+            video_name = os.path.basename(video_file)
+            latency_log = os.path.join(args.dataset_rootdir, args.reference_content, 'log', video_name, model.name, cache_profile_name, device_id, 'latency.txt')
+            metadata_log = os.path.join(args.dataset_rootdir, args.reference_content, 'log', video_name, model.name, cache_profile_name, device_id, 'metadata.txt')
 
-                #anchor point
-                if int(metadata_result[2]) == 1:
-                    anchor_point.append(float(latency_result[2]))
-                #non-anchor frame
-                elif int(metadata_result[2]) == 0:
-                    non_anchor_frame.append(float(latency_result[2]))
-                else:
-                    raise RuntimeError
+            with open(latency_log, 'r') as f0, open(metadata_log, 'r') as f1:
+                latency_lines = f0.readlines()
+                metadata_lines = f1.readlines()
 
-            latency_dict[model.name] = {}
-            latency_dict[model.name]['anchor_point'] = np.average(anchor_point)
-            latency_dict[model.name]['non_anchor_frame'] = np.average(non_anchor_frame)
+                for latency_line, metadata_line in zip(latency_lines, metadata_lines):
+                    latency_result = latency_line.strip().split('\t')
+                    metadata_result = metadata_line.strip().split('\t')
 
-            print('Device {}: Anchor point {:.2f}ms, Non-anchor frame {:.2f}ms'.format(args.device_id, np.average(anchor_point), np.average(non_anchor_frame)))
+                    #anchor point
+                    if int(metadata_result[2]) == 1:
+                        anchor_point.append(float(latency_result[2]))
+                    #non-anchor frame
+                    elif int(metadata_result[2]) == 0:
+                        non_anchor_frame.append(float(latency_result[2]))
+                    else:
+                        raise RuntimeError
 
+                latency_dict[device_id][model.name] = {}
+                latency_dict[device_id][model.name]['anchor_point'] = np.average(anchor_point)
+                latency_dict[device_id][model.name]['non_anchor_frame'] = np.average(non_anchor_frame)
+
+                print('Device {}: Anchor point {:.2f}ms, Non-anchor frame {:.2f}ms'.format(device_id, np.average(anchor_point), np.average(non_anchor_frame)))
+
+
+    selected_model = {}
+    for device_id in args.device_id:
+        for content in args.content:
+            selected_model[content] = {}
+            video_file = os.path.abspath(glob.glob(os.path.join(args.dataset_rootdir, content,  'video', '{}p*'.format(args.lr_resolution)))[0])
+            video_name = os.path.basename(video_file)
+            fps = profile_video(video_file)['frame_rate']
+            log = os.path.join(args.dataset_rootdir, content, 'log', video_name, model.name, '{}_{}'.format(APS_NEMO.NAME1, args.threshold), 'quality.txt')
+
+            with open(log, 'r') as f:
+                lines = f.readlines()
+                for model in models:
+                    is_real_time = True
+                    for line in lines:
+                        line = line.strip().split('\t')
+                        num_anchor_points = int(line[1])
+                        max_latency = 0
+                        latency = num_anchor_points * latency_dict[device_id][model.name]['anchor_point'] + (args.gop - num_anchor_points) * latency_dict[device_id][model.name]['non_anchor_frame']
+                        if latency > (args.gop / fps) * 1000:
+                            is_real_time = False
+                            break
+
+                    if is_real_time is True:
+                        selected_model[content][device_id] = {}
+                        selected_model[content][device_id]['num_blocks'] = model.num_blocks
+                        selected_model[content][device_id]['num_filters'] = model.num_filters
+                        selected_model[content][device_id]['scale'] = model.scale
 
     for content in args.content:
-        selected_model = None
-        video_file = os.path.abspath(glob.glob(os.path.join(args.dataset_rootdir, content,  'video', '{}p*'.format(args.lr_resolution)))[0])
-        video_name = os.path.basename(video_file)
-        fps = profile_video(video_file)['frame_rate']
-        log = os.path.join(args.dataset_rootdir, content, 'log', video_name, model.name, '{}_{}'.format(APS_NEMO.NAME1, args.threshold), 'quality.txt')
+        json_file = os.path.join(args.dataset_rootdir, content, 'log', video_name, 'device_to_dnn.json')
 
-        with open(log, 'r') as f:
-            lines = f.readlines()
-            for line in lines:
-                line = line.strip().split('\t')
-                num_anchor_points = int(line[1])
-                for model in models:
-                    latency = num_anchor_points * latency_dict[model.name]['anchor_point'] + (args.gop - num_anchor_points) * latency_dict[model.name]['non_anchor_frame']
-                    if latency < (args.gop / fps) * 1000:
-                        selected_model = model
-
-        print('Content {}: Selected model {}'.format(content, selected_model.name))
-
-        #TODO: json file
+        with open(json_file, 'w') as f:
+            json.dump(selected_model[content], f)
--- cache_profile/nemo_s/script/quality_selector_nemo_s.sh
+++ cache_profile/nemo_s/script/quality_selector_nemo_s.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+python $MOBINAS_CODE_ROOT/cache_profile/nemo_s/quality_selector_nemo_s.py \
+                --dataset_rootdir $MOBINAS_DATA_ROOT \
+                --reference_content unboxing \
+                --content unboxing \
+                --vpxdec_file $MOBINAS_CODE_ROOT/third_party/libvpx/vpxdec \
+                --lr_resolution 480 \
+                --hr_resolution 1080 \
+                --num_blocks 8 8 8 \
+                --num_filters 4 9 18 \
+                --gop 120 \
+                --threshold 0.5 \
+                --device_id 7b7f59d1
+
 python $MOBINAS_CODE_ROOT/cache_profile/nemo_s/quality_selector_nemo_s.py \
                 --dataset_rootdir $MOBINAS_DATA_ROOT \
                 --reference_content unboxing \
@@ -8,6 +22,19 @@ python $MOBINAS_CODE_ROOT/cache_profile/nemo_s/quality_selector_nemo_s.py \
                 --hr_resolution 1080 \
                 --num_blocks 8 8 8 \
                 --num_filters 9 21 32 \
+                --gop 120 \
+                --threshold 0.5 \
+                --device_id 7b7f59d1
+
+python $MOBINAS_CODE_ROOT/cache_profile/nemo_s/quality_selector_nemo_s.py \
+                --dataset_rootdir $MOBINAS_DATA_ROOT \
+                --reference_content unboxing \
+                --content unboxing \
+                --vpxdec_file $MOBINAS_CODE_ROOT/third_party/libvpx/vpxdec \
+                --lr_resolution 360 \
+                --hr_resolution 1080 \
+                --num_blocks 8 8 8 \
+                --num_filters 8 18 29 \
                 --gop 120 \
                 --threshold 0.5 \
                 --device_id 7b7f59d1
