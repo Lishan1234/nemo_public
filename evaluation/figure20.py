0d310521b910e0eb594c76d8aef07ee32814b0f8
Hyunho Yeo <chaos5958@gmail.com>
2020-08-03 23:39:08 +0900

    Minor

---
+++ evaluation/figure20.py
@@ -0,0 +1,190 @@
+import argparse
+import os
+import glob
+import operator
+
+import numpy as np
+
+contents = ['product_review', 'how_to', 'vlogs', 'game_play', 'skit', 'haul', 'challenge','favorite', 'education',  'unboxing']
+indexes = [1, 2, 3]
+resolution = 240
+quality = 'high'
+device_name = 'xiaomi_mi9'
+
+num_blocks_info= {
+    'low': {
+        240: 4,
+        360: 4,
+        480: 4
+    },
+    'medium': {
+        240: 8,
+        360: 4,
+        480: 4
+    },
+    'high': {
+        240: 8,
+        360: 4,
+        480: 4
+    },
+}
+
+num_filters_info= {
+    'low': {
+        240: 9,
+        360: 8,
+        480: 4
+    },
+    'medium': {
+        240: 21,
+        360: 18,
+        480: 9
+    },
+    'high': {
+        240: 32,
+        360: 29,
+        480: 18
+    },
+}
+
+video_name_info = {
+    240: '240p_512kbps_s0_d300.webm',
+    360: '360p_1024kbps_s0_d300.webm',
+    480: '480p_1600kbps_s0_d300.webm',
+}
+
+algorithm_info = {
+    'low': 'nemo_0.5_8',
+    'medium': 'nemo_0.5_16',
+    'high': 'nemo_0.5_16'
+}
+
+def get_model_name(num_blocks, num_filters, resolution):
+    if resolution == 240:
+        scale = 4
+    elif resolution == 360:
+        scale = 3
+    elif resolution == 240:
+        scale = 2
+
+    return 'NEMO_S_B{}_F{}_S{}_deconv'.format(num_blocks, num_filters, scale)
+
+def load_num_anchor_points(log_path):
+    num_anchor_points = []
+
+    with open(log_path, 'r') as f:
+        lines = f.readlines()
+
+        for line in lines:
+            line = line.strip().split('\t')
+            num_anchor_points.append(float(line[1]))
+
+    return num_anchor_points
+
+def load_num_anchor_points(log_dir):
+    quality_log_file = os.path.join(log_dir, 'quality.txt')
+    num_anchor_points = []
+
+    with open(quality_log_file, 'r') as f:
+        quality_lines = f.readlines()
+        for quality_line in quality_lines:
+            quality_line = quality_line.strip().split('\t')
+            num_anchor_points.append(int(quality_line[1]))
+
+    return num_anchor_points
+
+def load_chunk_quality(log_dir, chunk_idx):
+    quality_log_file = os.path.join(log_dir, 'quality.txt')
+
+    with open(quality_log_file, 'r') as f:
+        quality_lines = f.readlines()
+
+        quality_line = quality_lines[chunk_idx].strip().split('\t')
+        return float(quality_line[1]), float(quality_line[2])
+
+def load_frame_quality(log_dir):
+    quality_log_file = os.path.join(log_dir, 'quality.txt')
+    quality = []
+
+    with open(quality_log_file, 'r') as f:
+        quality_lines = f.readlines()
+
+        for quality_line in quality_lines:
+            quality_line = quality_line.strip().split('\t')
+            quality.append(float(quality_line[1]))
+
+    return quality
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    #directory, path
+    parser.add_argument('--data_dir', type=str, required=True)
+
+    #setting
+    quality = 'high'
+    resolution = 240
+
+    #log
+    log_dir = os.path.join(args.dataset_rootdir, 'evaluation')
+    os.makedirs(log_dir, exist_ok=True)
+    log_file_0 = os.path.join(log_dir, 'figure19_a.txt')
+    log_file_1 = os.path.join(log_dir, 'figure19_b.txt')
+
+    with open(log_file_0, 'w') as f_0, open(log_file_1, 'w') as f_1:
+        #video, directory
+        content = 'education1'
+        video_dir = os.path.join(args.data_dir, content, 'video')
+        video_name = video_name_info[resolution]
+        log_dir = os.path.join(args.data_dir, content, 'log')
+
+        num_blocks = num_blocks_info[quality][resolution]
+        num_filters = num_filters_info[quality][resolution]
+        model_name = get_model_name(num_blocks, num_filters, resolution)
+
+
+        #chunk quality
+        for i in range(75):
+            postfix = 'chunk{:04d}'.format(i)
+            cache_profile_name = '{}_{}'.format(APS_NEMO.NAME1, args.threshold)
+            cache_log_dir = os.path.join(log_dir, lr_video_name, nemo_s.name, cache_profile_name, postfix)
+            nemo_chunk_quality, dnn_chunk_quality = load_chunk_quality(cache_log_dir, -1)
+
+            cache_profile_name = '{}_{}'.format(APS_Uniform.NAME1, args.threshold)
+            cache_log_dir = os.path.join(log_dir, lr_video_name, nemo_s.name, cache_profile_name, postfix)
+            uniform_chunk_quality, _ = load_chunk_quality(cache_log_dir, num_anchor_points[i] - 1)
+
+            cache_profile_name = '{}_{}'.format(APS_Random.NAME1, args.threshold)
+            cache_log_dir = os.path.join(log_dir, lr_video_name, nemo_s.name, cache_profile_name, postfix)
+            random_chunk_quality, _ = load_chunk_quality(cache_log_dir, num_anchor_points[i] - 1)
+
+            f_0.write('{}\t{:.2f}\t{:.2f}\t{:.2f}\t{:.2f}\n'.format(i, nemo_chunk_quality, uniform_chunk_quality, random_chunk_quality, dnn_chunk_quality))
+
+            nemo_log_path = os.path.join(args.data_dir, content, 'log', video_name, model_name, 'quality_nemo_0.5.txt')
+            nemo_num_anchor_points.extend(load_num_anchor_points(nemo_log_path))
+            uniform_log_path = os.path.join(args.data_dir, content, 'log', video_name, model_name, 'quality_uniform_0.5.txt')
+            uniform_num_anchor_points.extend(load_num_anchor_points(uniform_log_path))
+            random_log_path = os.path.join(args.data_dir, content, 'log', video_name, model_name, 'quality_random_0.5.txt')
+            random_num_anchor_points.extend(load_num_anchor_points(random_log_path))
+
+        #frame quality
+        postfix = 'chunk0010'
+        cache_profile_name = '{}_{}.profile'.format(APS_NEMO.NAME1, args.threshold)
+        cache_log_dir = os.path.join(log_dir, lr_video_name, nemo_s.name, cache_profile_name, postfix)
+        nemo_frame_quality = load_frame_quality(cache_log_dir)
+
+        cache_profile_name = '{}_{}'.format(APS_Uniform.NAME1, num_anchor_points[10])
+        cache_log_dir = os.path.join(log_dir, lr_video_name, nemo_s.name, cache_profile_name, postfix)
+        uniform_frame_quality = load_frame_quality(cache_log_dir)
+
+        cache_profile_name = '{}_{}.tmp'.format(APS_Random.NAME1, num_anchor_points[10])
+        cache_log_dir = os.path.join(log_dir, lr_video_name, nemo_s.name, cache_profile_name, postfix)
+        random_frame_quality = load_frame_quality(cache_log_dir)
+
+        cache_log_dir = os.path.join(log_dir, lr_video_name, nemo_s.name, postfix)
+        dnn_frame_quality = load_frame_quality(cache_log_dir)
+
+        count = 0
+        for nq, uq, rq, dq in zip(nemo_frame_quality, uniform_frame_quality, random_frame_quality, dnn_frame_quality):
+            f_1.write('{}\t{:.2f}\t{:.2f}\t{:.2f}\t{:.2f}\n'.format(count, nq, uq, rq, dq))
+            count += 1
